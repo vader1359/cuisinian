2f749de3c5622c1b2afa7c3a67a376026967dfd1
vader1359 <vader1359@gmail.com>
2017-08-05 10:38:47 +0700

    Problem: @section.rb already add uniqueness but not works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resource :sections, only: [:show]
-
+  resource :sections, only: [:show] do
+    resource :dishes
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "contact" => "home#contact"
   get "welcome" => "home#welcome"
