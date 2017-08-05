28686c27e03987da6ac7644f7783a21f92a45953
vader1359 <vader1359@gmail.com>
2017-08-05 09:55:54 +0700

    Start adding database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sections/name'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "contact" => "home#contact"
   get "welcome" => "home#welcome"
